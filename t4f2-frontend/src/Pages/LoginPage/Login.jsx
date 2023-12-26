080c9ab86d8510958b4dbab8f28985d445e25a6f
chan_nie <126068623+chennielee@users.noreply.github.com>
2023-12-26 21:20:07 +0900

    feat: 비로그인 페이지

--- t4f2-frontend/src/Pages/LoginPage/Login.jsx
+++ t4f2-frontend/src/Pages/LoginPage/Login.jsx
@@ -1,7 +1,53 @@
 import React from "react";
+import * as S from "./Login.style";
+import Fade from "react-reveal/Fade";
 
 function Login() {
-  return <div>로그인 화면</div>;
+  return (
+    <S.Container>
+      <S.Login>Sign In</S.Login>
+
+      <S.MainTitle>
+        <S.ServiceTitle>Waggle!</S.ServiceTitle>
+        <S.ListDetail>Today's Topic</S.ListDetail>
+      </S.MainTitle>
+
+      <S.TodayList>
+        <Fade bottom delay={0}>
+          <S.List>
+            <S.PlayImg src="/images/today1.png" alt="img1"></S.PlayImg>
+            <S.PlayBar></S.PlayBar>
+            <S.PlayBarImg src="/images/playbar.png" alt="img2"></S.PlayBarImg>
+            <S.PlayExplain>살면서 겪은 역대급 흑역사 일화</S.PlayExplain>
+          </S.List>
+        </Fade>
+        <Fade bottom delay={500}>
+          <S.List>
+            <S.PlayImg src="/images/today2.png" alt="img3"></S.PlayImg>
+            <S.PlayBar></S.PlayBar>
+            <S.PlayBarImg src="/images/playbar.png" alt="img4"></S.PlayBarImg>
+            <S.PlayExplain>
+              수채화처럼 온 몸에 따스히 번지는 나의 봄
+            </S.PlayExplain>
+          </S.List>
+        </Fade>
+        <Fade bottom delay={1000}>
+          <S.List>
+            <S.PlayImg src="/images/today3.png" alt="im5"></S.PlayImg>
+            <S.PlayBar></S.PlayBar>
+            <S.PlayBarImg src="/images/playbar.png" alt="img6"></S.PlayBarImg>
+            <S.PlayExplain>친구가 이성으로 보이는 순간</S.PlayExplain>
+          </S.List>
+        </Fade>
+      </S.TodayList>
+      <S.ScrollCheck> 스크롤 체크입니다 </S.ScrollCheck>
+      <S.ScrollCheck> 스크롤 체크입니다 </S.ScrollCheck>
+      <S.ScrollCheck> 스크롤 체크입니다 </S.ScrollCheck>
+      <S.ScrollCheck> 스크롤 체크입니다 </S.ScrollCheck>
+      <S.ScrollCheck> 스크롤 체크입니다 </S.ScrollCheck>
+      <S.ScrollCheck> 스크롤 체크입니다 </S.ScrollCheck>
+    </S.Container>
+  );
 }
 
 export default Login;
---
+++ t4f2-frontend/src/Pages/LoginPage/Login.style.jsx
@@ -0,0 +1,66 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Login = styled.div`
+  width: 100px;
+  border: 1px solid black;
+  height: 50px;
+  color: #ba5a5a;
+`;
+
+export const MainTitle = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  margin-bottom: 10px;
+`;
+
+export const ServiceTitle = styled.div`
+  font-size: xxx-large;
+  font-weight: 700;
+`;
+
+export const ListDetail = styled.div`
+  font-size: larger;
+  font-weight: 800;
+  margin-top: 15px;
+  margin-bottom: 20px;
+`;
+
+export const TodayList = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  width: 100%;
+`;
+
+export const List = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  border: 1px solid black;
+`;
+export const PlayImg = styled.img`
+  width: 100%;
+  height: 60%;
+`;
+
+export const PlayBar = styled.div`
+  margin: 30px 20px 5px 20px;
+  border: 1px solid black;
+`;
+export const PlayBarImg = styled.img``;
+export const PlayExplain = styled.div`
+  font-weight: 800;
+`;
+export const ScrollCheck = styled.div`
+  font-weight: 700;
+  margin-top: 20px;
+`;
